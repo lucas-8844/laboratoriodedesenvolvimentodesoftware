e8398180ce0d7b0996e241207dd5b7bbc99941ad
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-09 15:04:40 -0300

    Update app.js

--- Frontend/public/src/app.js
+++ Frontend/public/src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AlunosPage from './pages/AlunosPage';
+import EmpresaPage from './pages/EmpresaPage';
+import ProfessorPage from './pages/ProfessorPage';
+import TransacaoPage from './pages/TransacaoPage';
 
 function App() {
     return (
@@ -8,10 +11,14 @@ function App() {
             <Routes>
                 <Route path="/" element={<AlunosPage />} />
                 <Route path="/alunos" element={<AlunosPage />} />
+                <Route path="/empresas" element={<EmpresaPage />} />
+                <Route path="/professores" element={<ProfessorPage />} />
+                <Route path="/transacoes" element={<TransacaoPage />} />
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
 
