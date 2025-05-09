dd30a6cc03dedbe9792b037d6b9620586e84a926
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-09 15:02:06 -0300

    Update api.js

--- Frontend/public/src/services/api.js
+++ Frontend/public/src/services/api.js
@@ -5,3 +5,33 @@ const api = axios.create({
 });
 
 export default api;
+
+import api from './api';
+
+export const getAlunos = async () => api.get('/alunos');
+export const createAluno = async (aluno) => api.post('/alunos', aluno);
+export const updateAluno = async (id, aluno) => api.put(`/alunos/${id}`, aluno);
+export const deleteAluno = async (id) => api.delete(`/alunos/${id}`);
+
+import api from './api';
+
+export const getEmpresas = async () => api.get('/empresas');
+export const createEmpresa = async (empresa) => api.post('/empresas', empresa);
+export const updateEmpresa = async (id, empresa) => api.put(`/empresas/${id}`, empresa);
+export const deleteEmpresa = async (id) => api.delete(`/empresas/${id}`);
+
+import api from './api';
+
+export const getProfessores = async () => api.get('/professores');
+export const createProfessor = async (professor) => api.post('/professores', professor);
+export const updateProfessor = async (id, professor) => api.put(`/professores/${id}`, professor);
+export const deleteProfessor = async (id) => api.delete(`/professores/${id}`);
+
+import api from './api';
+
+export const getTransacoes = async () => api.get('/transacoes');
+export const createTransacao = async (transacao) => api.post('/transacoes', transacao);
+export const updateTransacao = async (id, transacao) => api.put(`/transacoes/${id}`, transacao);
+export const deleteTransacao = async (id) => api.delete(`/transacoes/${id}`);
+
+
