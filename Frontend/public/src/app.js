42294ab69f6698c0f18eb489eb5603bb25f29b5f
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-15 11:50:08 -0300

    Update app.js

--- Frontend/public/src/app.js
+++ Frontend/public/src/app.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
 import AlunosPage from './pages/AlunosPage';
 import EmpresaPage from './pages/EmpresaPage';
 import ProfessorPage from './pages/ProfessorPage';
 import TransacaoPage from './pages/TransacaoPage';
+import EnvioMoedasPage from './pages/EnvioMoedasPage';
+import AlunoExtratoPage from './pages/AlunoExtratoPage';
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<AlunosPage />} />
-                <Route path="/alunos" element={<AlunosPage />} />
-                <Route path="/empresas" element={<EmpresaPage />} />
-                <Route path="/professores" element={<ProfessorPage />} />
-                <Route path="/transacoes" element={<TransacaoPage />} />
-            </Routes>
+            <Navbar />
+            <div className="container mt-4">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/alunos" element={<AlunosPage />} />
+                    <Route path="/empresas" element={<EmpresaPage />} />
+                    <Route path="/professores" element={<ProfessorPage />} />
+                    <Route path="/transacoes" element={<TransacaoPage />} />
+                    <Route path="/enviar-moedas" element={<EnvioMoedasPage />} />
+                    <Route path="/extrato" element={<AlunoExtratoPage alunoId={1} />} />
+                </Routes>
+            </div>
         </Router>
     );
 }
 
 export default App;
+
 
 
