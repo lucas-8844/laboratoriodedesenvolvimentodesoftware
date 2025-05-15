ef8ac8d97a156885e61aed479702d46b396fa56b
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-15 11:45:15 -0300

    Update TransacaoRepository.java

--- backend/src/main/java/moedaestudantil/repository/TransacaoRepository.java
+++ backend/src/main/java/moedaestudantil/repository/TransacaoRepository.java
@@ -2,7 +2,9 @@ package com.moedaestudantil.repository;
 
 import com.moedaestudantil.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+List<Transacao> findByAlunoId(Long alunoId);
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 }
+
 
