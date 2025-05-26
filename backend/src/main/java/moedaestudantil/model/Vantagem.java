a26e42252289e49671f9d2b91d75200076f5aea2
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-26 10:44:45 -0300

    Create Vantagem.java

---
+++ backend/src/main/java/moedaestudantil/model/Vantagem.java
@@ -0,0 +1,14 @@
+@Entity
+@Data
+public class Vantagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private Integer custoEmMoedas;
+
+    @ManyToOne
+    private Empresa empresa;
+}
