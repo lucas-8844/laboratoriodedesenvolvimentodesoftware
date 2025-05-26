2ab597bb241ba7d48ed94bdca9ae63bd6db04d99
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-26 10:46:13 -0300

    Create VantagemService.java

---
+++ backend/src/main/java/moedaestudantil/service/VantagemService.java
@@ -0,0 +1,17 @@
+@Service
+public class VantagemService {
+    @Autowired
+    private VantagemRepository vantagemRepository;
+
+    public Vantagem cadastrar(Vantagem vantagem) {
+        return vantagemRepository.save(vantagem);
+    }
+
+    public List<Vantagem> listar() {
+        return vantagemRepository.findAll();
+    }
+
+    public List<Vantagem> listarPorEmpresa(Long empresaId) {
+        return vantagemRepository.findByEmpresaId(empresaId);
+    }
+}
