5caece92fccc02986d162cddf9e2b7ba838149a9
Lucas Carvalho <105749994+lucas-8844@users.noreply.github.com>
2025-05-30 19:49:50 -0300

    Update AlunoService.js

--- Frontend/public/src/services/AlunoService.js
+++ Frontend/public/src/services/AlunoService.js
@@ -2,3 +2,6 @@ import api from './api';
 
 export const enviarMoedas = async (transacao) => api.post('/transacoes/enviar', transacao);
 export const consultarExtrato = async (id) => api.get(`/alunos/${id}/extrato`);
+export const resgatarVantagem = async (alunoId, vantagemId) => {
+  return api.post('/transacoes/resgatar', { alunoId, vantagemId });
+};
